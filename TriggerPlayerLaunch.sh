ef60bb7942e13eaa3f97e575fc4b818f90097ca3
JesseCake <52616933+JesseCake@users.noreply.github.com>
2019-09-13 14:59:12 +1000

    Update TriggerPlayerLaunch.sh

--- TriggerPlayerLaunch.sh
+++ TriggerPlayerLaunch.sh
@@ -16,4 +16,4 @@ setterm -cursor off > /dev/tty1
 #wipe out the screen (cleared, black), redirect output to null
 dd if=/dev/zero of=/dev/fb0 > /dev/null 2>&1
 #start main python script in background and redirect output to null
-python3 /home/pi/TriggeredVideoPlayer_NetworkUDPTrigger_v6.py > /dev/null 2>&1 &
+python3 /home/pi/TriggeredVideoPlayer_NetworkUDPTrigger_v7.py > /dev/null 2>&1 &
